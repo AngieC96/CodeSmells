4593e354dca1e3503a3e90c0ad4c0d7b4df23db6
Angela Carraro <angela_carraro@hotmail.it>
2020-12-08 16:02:25 +0100

    Refactoring: extracting method isRowFull()

--- java/src/main/java/codesmells/Game.java
+++ java/src/main/java/codesmells/Game.java
@@ -27,10 +27,7 @@ public class Game {
     }
 
     public char Winner() {
-        //if the positions in first row are taken
-        if (_board.TileAt(0, 0).Symbol != ' ' &&
-                _board.TileAt(0, 1).Symbol != ' ' &&
-                _board.TileAt(0, 2).Symbol != ' ') {
+        if (isRowFull(0)) {
             //if first row is full with same symbol
             if (_board.TileAt(0, 0).Symbol ==
                     _board.TileAt(0, 1).Symbol &&
@@ -39,10 +36,7 @@ public class Game {
             }
         }
 
-        //if the positions in first row are taken
-        if (_board.TileAt(1, 0).Symbol != ' ' &&
-                _board.TileAt(1, 1).Symbol != ' ' &&
-                _board.TileAt(1, 2).Symbol != ' ') {
+        if (isRowFull(1)) {
             //if middle row is full with same symbol
             if (_board.TileAt(1, 0).Symbol ==
                     _board.TileAt(1, 1).Symbol &&
@@ -52,10 +46,7 @@ public class Game {
             }
         }
 
-        //if the positions in first row are taken
-        if (_board.TileAt(2, 0).Symbol != ' ' &&
-                _board.TileAt(2, 1).Symbol != ' ' &&
-                _board.TileAt(2, 2).Symbol != ' ') {
+        if (isRowFull(2)) {
             //if middle row is full with same symbol
             if (_board.TileAt(2, 0).Symbol ==
                     _board.TileAt(2, 1).Symbol &&
@@ -66,6 +57,12 @@ public class Game {
         }
 
         return ' ';
+    }
+
+    private boolean isRowFull(int i) {
+        return _board.TileAt(i, 0).Symbol != ' ' &&
+                _board.TileAt(i, 1).Symbol != ' ' &&
+                _board.TileAt(i, 2).Symbol != ' ';
     }
 }
 
