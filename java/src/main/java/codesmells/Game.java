3c222e2f1fb90d29b39d74dad7e1ea84f4b157f2
Angela Carraro <angela_carraro@hotmail.it>
2020-12-08 16:07:36 +0100

    Refactoring: extracting method isRowFullWithSameSymbol()

--- java/src/main/java/codesmells/Game.java
+++ java/src/main/java/codesmells/Game.java
@@ -28,35 +28,30 @@ public class Game {
 
     public char Winner() {
         if (isRowFull(0)) {
-            //if first row is full with same symbol
-            if (_board.TileAt(0, 0).Symbol ==
-                    _board.TileAt(0, 1).Symbol &&
-                    _board.TileAt(0, 2).Symbol == _board.TileAt(0, 1).Symbol) {
+            if (isRowFullWithSameSymbol(0)) {
                 return _board.TileAt(0, 0).Symbol;
             }
         }
 
         if (isRowFull(1)) {
-            //if middle row is full with same symbol
-            if (_board.TileAt(1, 0).Symbol ==
-                    _board.TileAt(1, 1).Symbol &&
-                    _board.TileAt(1, 2).Symbol ==
-                            _board.TileAt(1, 1).Symbol) {
+            if (isRowFullWithSameSymbol(1)) {
                 return _board.TileAt(1, 0).Symbol;
             }
         }
 
         if (isRowFull(2)) {
-            //if middle row is full with same symbol
-            if (_board.TileAt(2, 0).Symbol ==
-                    _board.TileAt(2, 1).Symbol &&
-                    _board.TileAt(2, 2).Symbol ==
-                            _board.TileAt(2, 1).Symbol) {
+            if (isRowFullWithSameSymbol(2)) {
                 return _board.TileAt(2, 0).Symbol;
             }
         }
 
         return ' ';
+    }
+
+    private boolean isRowFullWithSameSymbol(int i) {
+        return _board.TileAt(i, 0).Symbol ==
+                _board.TileAt(i, 1).Symbol &&
+                _board.TileAt(i, 2).Symbol == _board.TileAt(i, 1).Symbol;
     }
 
     private boolean isRowFull(int i) {
