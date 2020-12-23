38e78c3002eac8a7c36980eecc1fdd9e0a28bfee
Angela Carraro <angela_carraro@hotmail.it>
2020-12-23 18:39:56 +0100

    A better hashCode() method

--- java/src/main/java/codesmells/Position.java
+++ java/src/main/java/codesmells/Position.java
@@ -29,24 +29,33 @@ public class Position {
 
     @Override
     public boolean equals(Object o) {
-        // self check
+        // self check: checking if both the object references
+        // are referring to the same object.
         if (this == o)
             return true;
-        // null check
+        // null check: checking if the object reference is null.
         if (o == null)
             return false;
-        // type check and cast
+        // type check and cast: it checks if the argument is
+        // of the type Position by comparing the classes
+        // of the passed argument and this object.
+        // if(!(obj instanceof Position)) return false; ---> avoid.
         if (getClass() != o.getClass())
             return false;
+        // type casting of the argument.
         Position p = (Position) o;
-        // field comparison
+        // field comparison: comparing the state of argument
+        // with the state of 'this' Object.
         return this.getX() == p.getX() &&
                 this.getY() == p.getY();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(X, Y);
+        int prime = 31;
+        int result = 1;
+        result = prime * (prime + X) + Y;
+        return result;
     }
 
 }
